3b9ef98241c2953071d610fbc8c6533e2716e337
tarynmitchell <tarynmitchll@gmail.com>
2024-06-04 18:17:04 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Taryn Mitchell and is{" "}
+        <a
+          href="https://github.com/tarynmitchell/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
