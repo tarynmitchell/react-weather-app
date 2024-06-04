117c9dd7c262a2223f22c0aba2f9f86d3ea9c637
tarynmitchell <tarynmitchll@gmail.com>
2024-06-04 18:35:36 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Taryn Mitchell and is{" "}
-        <a
-          href="https://github.com/tarynmitchell/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Taryn Mitchell and is{" "}
+          <a
+            href="https://github.com/tarynmitchell/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
