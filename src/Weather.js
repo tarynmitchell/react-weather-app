71c4669e5302d8de0ee4111e6ba2e705f85a8d16
tarynmitchell <tarynmitchll@gmail.com>
2024-06-11 18:32:48 +0100

    Updated spelling error

--- src/Weather.js
+++ src/Weather.js
@@ -52,7 +52,7 @@ export default function Weather(props) {
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="search" clasName="button-primary" />
+              <input type="submit" value="search" className="button-primary" />
             </div>
           </div>
         </form>
