b26e74373dc171dc517018e4925ec33dc9f3dd12
tarynmitchell <tarynmitchll@gmail.com>
2024-06-11 18:11:37 +0100

    Duplicated the daily forecast to show more days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,6 +20,21 @@ export default function WeatherForecast(props) {
           <div className="col">
             <WeatherForecastDay data={forecast[0]} />
           </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[5]} />
+          </div>
         </div>
       </div>
     );
